23afd0e4cb2926e5a84f9889367b78a246c5e335
Oliver <grantfromgeorgia@gmail.com>
2024-02-29 00:01:48 +0300

    first

---
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -0,0 +1,24 @@
+package exercise;
+
+// BEGIN
+public class Segment {
+    private Point start;
+    private Point end;
+    Segment(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Point getBeginPoint() {
+        return start;
+    }
+
+    public Point getEndPoint() {
+        return end;
+    }
+
+    public Point getMidPoint() {
+        return new Point((start.getX() + end.getX()) / 2, (start.getY() + end.getY()) / 2);
+    }
+}
+// END
