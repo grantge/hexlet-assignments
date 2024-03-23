a114c7d0fef414fac02e78a665bbf12469a36588
Oliver <grantfromgeorgia@gmail.com>
2024-03-23 20:22:35 +0300

    first

---
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -0,0 +1,26 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+// BEGIN
+
+// END
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+        var app = Javalin.create()
+                .get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"))
+                .start(7070);
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
